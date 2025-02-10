ba77d844be520bd8aee46688f009c8c1b6407d37
Everesh <everesh@proton.me>
2025-02-10 23:38:23 +0100

    Add Join Button

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :event, only: [ :index, :new, :create, :show ]
+  resources :event, only: [ :index, :new, :create, :show ] do
+    member do
+      post "join", to: "attendance#create"
+    end
+  end
   resources :user, only: :show
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
